afa5a21bac6b3643492d7da3f6bbf0521806bd09
skuhlmann <samkuhlmann@gmail.com>
2019-09-26 09:46:03 -0600

    Initial commit

---
+++ src/moloch-mapping.ts
@@ -0,0 +1,278 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import {
+  Moloch as Contract,
+  SummonComplete,
+  SubmitProposal,
+  SubmitVote,
+  ProcessProposal,
+  Ragequit,
+  Abort,
+  UpdateDelegateKey
+} from "./types/Moloch/Moloch";
+import { Proposal, Member, Vote, Applicant } from "./types/schema";
+
+export function handleSummonComplete(event: SummonComplete): void {
+  let memberId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.summoner.toHex());
+
+  // let member = new Member(event.params.summoner.toHex());
+  let member = new Member(memberId);
+  member.molochAddress = event.address;
+  member.delegateKey = event.params.summoner;
+  member.shares = event.params.shares;
+  member.isActive = true;
+  member.tokenTribute = BigInt.fromI32(0);
+  member.didRagequit = false;
+  member.votes = new Array<string>();
+  member.submissions = new Array<string>();
+  member.save();
+}
+
+export function handleSubmitProposal(event: SubmitProposal): void {
+  // get information directly from contract
+  // struct Proposal {
+  //     address proposer; // the member who submitted the proposal
+  //     address applicant; // the applicant who wishes to become a member - this key will be used for withdrawals
+  //     uint256 sharesRequested; // the # of shares the applicant is requesting
+  //     uint256 startingPeriod; // the period in which voting can start for this proposal
+  //     uint256 yesVotes; // the total number of YES votes for this proposal
+  //     uint256 noVotes; // the total number of NO votes for this proposal
+  //     bool processed; // true only if the proposal has been processed
+  //     bool didPass; // true only if the proposal passed
+  //     bool aborted; // true only if applicant calls "abort" fn before end of voting period
+  //     uint256 tokenTribute; // amount of tokens offered as tribute
+  //     string details; // proposal details - could be IPFS hash, plaintext, or JSON
+  //     uint256 maxTotalSharesAtYesVote; // the maximum # of total shares encountered at a yes vote on this proposal
+  //     mapping (address => Vote) votesByMember; // the votes on this proposal by each member
+  // }
+  let contract = Contract.bind(event.address);
+  let proposalFromContract = contract.proposalQueue(event.params.proposalIndex);
+  let startingPeriod = proposalFromContract.value3;
+  let details = proposalFromContract.value10;
+
+  // let proposal = new Proposal(event.params.proposalIndex.toString());
+  let proposalId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.proposalIndex.toString());
+
+  let proposal = new Proposal(proposalId);
+  proposal.molochAddress = event.address;
+  proposal.timestamp = event.block.timestamp.toString();
+  proposal.proposalIndex = event.params.proposalIndex;
+  proposal.startingPeriod = startingPeriod;
+  proposal.delegateKey = event.params.delegateKey;
+  proposal.member = event.params.memberAddress.toHex();
+  proposal.memberAddress = event.params.memberAddress;
+  proposal.applicant = event.params.applicant.toHex();
+  proposal.applicantAddress = event.params.applicant;
+  proposal.tokenTribute = event.params.tokenTribute;
+  proposal.sharesRequested = event.params.sharesRequested;
+  proposal.yesVotes = BigInt.fromI32(0);
+  proposal.noVotes = BigInt.fromI32(0);
+  proposal.processed = false;
+  proposal.didPass = false;
+  proposal.aborted = false;
+  proposal.votes = new Array<string>();
+  proposal.details = details;
+  proposal.save();
+
+  // let applicant = new Applicant(event.params.applicant.toHex());
+  let applicantId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.applicant.toHex());
+
+  let applicant = new Applicant(applicantId);
+  applicant.molochAddress = event.address;
+  applicant.timestamp = event.block.timestamp.toString();
+  applicant.proposalIndex = event.params.proposalIndex;
+  applicant.delegateKey = event.params.delegateKey;
+  applicant.member = event.params.memberAddress.toHex();
+  applicant.memberAddress = event.params.memberAddress;
+  applicant.applicantAddress = event.params.applicant;
+  applicant.tokenTribute = event.params.tokenTribute;
+  applicant.sharesRequested = event.params.sharesRequested;
+  applicant.didPass = false;
+  applicant.aborted = false;
+  applicant.votes = new Array<string>();
+  applicant.proposal = event.params.proposalIndex.toString();
+  applicant.save();
+
+  let memberId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.memberAddress.toHex());
+
+  let member = Member.load(memberId);
+  let submission = event.params.proposalIndex.toString();
+  let memberSubmissions = member.submissions;
+  memberSubmissions.push(submission);
+  member.submissions = memberSubmissions;
+  member.save();
+}
+
+export function handleSubmitVote(event: SubmitVote): void {
+  let voteID = event.params.memberAddress
+    .toHex()
+    .concat("-")
+    .concat(event.params.proposalIndex.toString());
+
+  let vote = new Vote(voteID);
+  vote.molochAddress = event.address;
+  vote.timestamp = event.block.timestamp.toString();
+  vote.proposalIndex = event.params.proposalIndex;
+  vote.delegateKey = event.params.delegateKey;
+  vote.memberAddress = event.params.memberAddress;
+  vote.uintVote = event.params.uintVote;
+  vote.proposal = event.params.proposalIndex.toString();
+  vote.member = event.params.memberAddress.toHex();
+  vote.save();
+
+  let proposalId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.proposalIndex.toString());
+
+  // let proposal = Proposal.load(event.params.proposalIndex.toString());
+  let proposal = Proposal.load(proposalId);
+  if (event.params.uintVote == 1) {
+    proposal.yesVotes = proposal.yesVotes.plus(BigInt.fromI32(1));
+  }
+  if (event.params.uintVote == 2) {
+    proposal.noVotes = proposal.noVotes.plus(BigInt.fromI32(1));
+  }
+
+  let proposalVotes = proposal.votes;
+  proposalVotes.push(voteID);
+  proposal.votes = proposalVotes;
+  proposal.save();
+
+  // let applicant = Applicant.load(proposal.applicant);
+  let applicantId = event.address
+    .toHex()
+    .concat("-")
+    .concat(proposal.applicant);
+
+  let applicant = Applicant.load(applicantId);
+  let applicantVotes = applicant.votes;
+  applicantVotes.push(voteID);
+  applicant.votes = applicantVotes;
+  applicant.save();
+
+  let memberId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.memberAddress.toHex());
+
+  // let member = Member.load(event.params.memberAddress.toHex());
+  let member = Member.load(memberId);
+  let memberVotes = member.votes;
+  memberVotes.push(voteID);
+  member.votes = memberVotes;
+  member.save();
+}
+
+export function handleProcessProposal(event: ProcessProposal): void {
+  let proposalId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.proposalIndex.toString());
+
+  // let proposal = Proposal.load(event.params.proposalIndex.toString());
+  let proposal = Proposal.load(proposalId);
+  proposal.applicant = event.params.applicant.toHex();
+  proposal.memberAddress = event.params.memberAddress;
+  proposal.tokenTribute = event.params.tokenTribute;
+  proposal.sharesRequested = event.params.sharesRequested;
+  proposal.didPass = event.params.didPass;
+  proposal.processed = true;
+  proposal.save();
+
+  if (event.params.didPass) {
+    let applicantId = event.address
+      .toHex()
+      .concat("-")
+      .concat(event.params.applicant.toHex());
+
+    // let applicant = Applicant.load(event.params.applicant.toHex());
+    let applicant = Applicant.load(applicantId);
+    applicant.didPass = true;
+    applicant.save();
+
+    let memberId = event.address
+      .toHex()
+      .concat("-")
+      .concat(event.params.applicant.toHex());
+
+    let member = Member.load(memberId);
+    // let member = Member.load(event.params.applicant.toHex());
+    if (member == null) {
+      let newMember = new Member(memberId);
+      newMember.molochAddress = event.address;
+      newMember.delegateKey = event.params.applicant;
+      newMember.shares = event.params.sharesRequested;
+      newMember.isActive = true;
+      newMember.tokenTribute = event.params.tokenTribute;
+      newMember.didRagequit = false;
+      newMember.votes = new Array<string>();
+      newMember.submissions = new Array<string>();
+      newMember.save();
+    } else {
+      member.shares = member.shares.plus(event.params.sharesRequested);
+      member.tokenTribute = member.tokenTribute.plus(event.params.tokenTribute);
+      member.save();
+    }
+  }
+}
+
+export function handleRagequit(event: Ragequit): void {
+  let memberId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.memberAddress.toHex());
+
+  // let member = Member.load(event.params.memberAddress.toHex());
+  let member = Member.load(memberId);
+  member.shares = member.shares.minus(event.params.sharesToBurn);
+  if (member.shares.equals(new BigInt(0))) {
+    member.isActive = false;
+  }
+  member.save();
+}
+
+export function handleAbort(event: Abort): void {
+  let proposalId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.proposalIndex.toString());
+
+  let proposal = Proposal.load(proposalId);
+  // let proposal = Proposal.load(event.params.proposalIndex.toString());
+  proposal.aborted = true;
+  proposal.save();
+
+  let applicantId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.applicantAddress.toHex());
+
+  let applicant = Applicant.load(applicantId);
+  // let applicant = Applicant.load(event.params.applicantAddress.toHex());
+  applicant.aborted = true;
+  applicant.save();
+}
+
+export function handleUpdateDelegateKey(event: UpdateDelegateKey): void {
+  let memberId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.memberAddress.toHex());
+
+  let member = Member.load(memberId);
+  // let member = Member.load(event.params.memberAddress.toHex());
+  member.delegateKey = event.params.newDelegateKey;
+  member.save();
+}
---
+++ src/types/schema.ts
@@ -0,0 +1,579 @@
+import {
+  TypedMap,
+  Entity,
+  Value,
+  ValueKind,
+  store,
+  Address,
+  Bytes,
+  BigInt
+} from "@graphprotocol/graph-ts";
+
+export class Vote extends Entity {
+  constructor(id: string) {
+    super();
+    this.entries = new Array(0);
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Vote entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Vote entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Vote", id.toString(), this);
+  }
+
+  static load(id: string): Vote | null {
+    return store.get("Vote", id) as Vote | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get timestamp(): string {
+    let value = this.get("timestamp");
+    return value.toString();
+  }
+
+  set timestamp(value: string) {
+    this.set("timestamp", Value.fromString(value));
+  }
+
+  get proposalIndex(): BigInt {
+    let value = this.get("proposalIndex");
+    return value.toBigInt();
+  }
+
+  set proposalIndex(value: BigInt) {
+    this.set("proposalIndex", Value.fromBigInt(value));
+  }
+
+  get delegateKey(): Bytes {
+    let value = this.get("delegateKey");
+    return value.toBytes();
+  }
+
+  set delegateKey(value: Bytes) {
+    this.set("delegateKey", Value.fromBytes(value));
+  }
+
+  get memberAddress(): Bytes {
+    let value = this.get("memberAddress");
+    return value.toBytes();
+  }
+
+  set memberAddress(value: Bytes) {
+    this.set("memberAddress", Value.fromBytes(value));
+  }
+
+  get uintVote(): i32 {
+    let value = this.get("uintVote");
+    return value.toI32();
+  }
+
+  set uintVote(value: i32) {
+    this.set("uintVote", Value.fromI32(value));
+  }
+
+  get proposal(): string {
+    let value = this.get("proposal");
+    return value.toString();
+  }
+
+  set proposal(value: string) {
+    this.set("proposal", Value.fromString(value));
+  }
+
+  get member(): string {
+    let value = this.get("member");
+    return value.toString();
+  }
+
+  set member(value: string) {
+    this.set("member", Value.fromString(value));
+  }
+
+  get molochAddress(): Bytes {
+    let value = this.get("molochAddress");
+    return value.toBytes();
+  }
+
+  set molochAddress(value: Bytes) {
+    this.set("molochAddress", Value.fromBytes(value));
+  }
+}
+
+export class Proposal extends Entity {
+  constructor(id: string) {
+    super();
+    this.entries = new Array(0);
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Proposal entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Proposal entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Proposal", id.toString(), this);
+  }
+
+  static load(id: string): Proposal | null {
+    return store.get("Proposal", id) as Proposal | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get timestamp(): string {
+    let value = this.get("timestamp");
+    return value.toString();
+  }
+
+  set timestamp(value: string) {
+    this.set("timestamp", Value.fromString(value));
+  }
+
+  get proposalIndex(): BigInt {
+    let value = this.get("proposalIndex");
+    return value.toBigInt();
+  }
+
+  set proposalIndex(value: BigInt) {
+    this.set("proposalIndex", Value.fromBigInt(value));
+  }
+
+  get startingPeriod(): BigInt {
+    let value = this.get("startingPeriod");
+    return value.toBigInt();
+  }
+
+  set startingPeriod(value: BigInt) {
+    this.set("startingPeriod", Value.fromBigInt(value));
+  }
+
+  get delegateKey(): Bytes {
+    let value = this.get("delegateKey");
+    return value.toBytes();
+  }
+
+  set delegateKey(value: Bytes) {
+    this.set("delegateKey", Value.fromBytes(value));
+  }
+
+  get member(): string {
+    let value = this.get("member");
+    return value.toString();
+  }
+
+  set member(value: string) {
+    this.set("member", Value.fromString(value));
+  }
+
+  get memberAddress(): Bytes {
+    let value = this.get("memberAddress");
+    return value.toBytes();
+  }
+
+  set memberAddress(value: Bytes) {
+    this.set("memberAddress", Value.fromBytes(value));
+  }
+
+  get applicant(): string {
+    let value = this.get("applicant");
+    return value.toString();
+  }
+
+  set applicant(value: string) {
+    this.set("applicant", Value.fromString(value));
+  }
+
+  get applicantAddress(): Bytes {
+    let value = this.get("applicantAddress");
+    return value.toBytes();
+  }
+
+  set applicantAddress(value: Bytes) {
+    this.set("applicantAddress", Value.fromBytes(value));
+  }
+
+  get tokenTribute(): BigInt {
+    let value = this.get("tokenTribute");
+    return value.toBigInt();
+  }
+
+  set tokenTribute(value: BigInt) {
+    this.set("tokenTribute", Value.fromBigInt(value));
+  }
+
+  get sharesRequested(): BigInt {
+    let value = this.get("sharesRequested");
+    return value.toBigInt();
+  }
+
+  set sharesRequested(value: BigInt) {
+    this.set("sharesRequested", Value.fromBigInt(value));
+  }
+
+  get yesVotes(): BigInt {
+    let value = this.get("yesVotes");
+    return value.toBigInt();
+  }
+
+  set yesVotes(value: BigInt) {
+    this.set("yesVotes", Value.fromBigInt(value));
+  }
+
+  get noVotes(): BigInt {
+    let value = this.get("noVotes");
+    return value.toBigInt();
+  }
+
+  set noVotes(value: BigInt) {
+    this.set("noVotes", Value.fromBigInt(value));
+  }
+
+  get processed(): boolean {
+    let value = this.get("processed");
+    return value.toBoolean();
+  }
+
+  set processed(value: boolean) {
+    this.set("processed", Value.fromBoolean(value));
+  }
+
+  get didPass(): boolean {
+    let value = this.get("didPass");
+    return value.toBoolean();
+  }
+
+  set didPass(value: boolean) {
+    this.set("didPass", Value.fromBoolean(value));
+  }
+
+  get aborted(): boolean {
+    let value = this.get("aborted");
+    return value.toBoolean();
+  }
+
+  set aborted(value: boolean) {
+    this.set("aborted", Value.fromBoolean(value));
+  }
+
+  get votes(): Array<string> {
+    let value = this.get("votes");
+    return value.toStringArray();
+  }
+
+  set votes(value: Array<string>) {
+    this.set("votes", Value.fromStringArray(value));
+  }
+
+  get details(): string {
+    let value = this.get("details");
+    return value.toString();
+  }
+
+  set details(value: string) {
+    this.set("details", Value.fromString(value));
+  }
+
+  get maxTotalSharesAtYesVote(): BigInt {
+    let value = this.get("maxTotalSharesAtYesVote");
+    return value.toBigInt();
+  }
+
+  set maxTotalSharesAtYesVote(value: BigInt) {
+    this.set("maxTotalSharesAtYesVote", Value.fromBigInt(value));
+  }
+
+  get molochAddress(): Bytes {
+    let value = this.get("molochAddress");
+    return value.toBytes();
+  }
+
+  set molochAddress(value: Bytes) {
+    this.set("molochAddress", Value.fromBytes(value));
+  }
+}
+
+export class Applicant extends Entity {
+  constructor(id: string) {
+    super();
+    this.entries = new Array(0);
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Applicant entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Applicant entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Applicant", id.toString(), this);
+  }
+
+  static load(id: string): Applicant | null {
+    return store.get("Applicant", id) as Applicant | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get timestamp(): string {
+    let value = this.get("timestamp");
+    return value.toString();
+  }
+
+  set timestamp(value: string) {
+    this.set("timestamp", Value.fromString(value));
+  }
+
+  get proposalIndex(): BigInt {
+    let value = this.get("proposalIndex");
+    return value.toBigInt();
+  }
+
+  set proposalIndex(value: BigInt) {
+    this.set("proposalIndex", Value.fromBigInt(value));
+  }
+
+  get delegateKey(): Bytes {
+    let value = this.get("delegateKey");
+    return value.toBytes();
+  }
+
+  set delegateKey(value: Bytes) {
+    this.set("delegateKey", Value.fromBytes(value));
+  }
+
+  get member(): string {
+    let value = this.get("member");
+    return value.toString();
+  }
+
+  set member(value: string) {
+    this.set("member", Value.fromString(value));
+  }
+
+  get memberAddress(): Bytes {
+    let value = this.get("memberAddress");
+    return value.toBytes();
+  }
+
+  set memberAddress(value: Bytes) {
+    this.set("memberAddress", Value.fromBytes(value));
+  }
+
+  get applicantAddress(): Bytes {
+    let value = this.get("applicantAddress");
+    return value.toBytes();
+  }
+
+  set applicantAddress(value: Bytes) {
+    this.set("applicantAddress", Value.fromBytes(value));
+  }
+
+  get tokenTribute(): BigInt {
+    let value = this.get("tokenTribute");
+    return value.toBigInt();
+  }
+
+  set tokenTribute(value: BigInt) {
+    this.set("tokenTribute", Value.fromBigInt(value));
+  }
+
+  get sharesRequested(): BigInt {
+    let value = this.get("sharesRequested");
+    return value.toBigInt();
+  }
+
+  set sharesRequested(value: BigInt) {
+    this.set("sharesRequested", Value.fromBigInt(value));
+  }
+
+  get didPass(): boolean {
+    let value = this.get("didPass");
+    return value.toBoolean();
+  }
+
+  set didPass(value: boolean) {
+    this.set("didPass", Value.fromBoolean(value));
+  }
+
+  get aborted(): boolean {
+    let value = this.get("aborted");
+    return value.toBoolean();
+  }
+
+  set aborted(value: boolean) {
+    this.set("aborted", Value.fromBoolean(value));
+  }
+
+  get votes(): Array<string> {
+    let value = this.get("votes");
+    return value.toStringArray();
+  }
+
+  set votes(value: Array<string>) {
+    this.set("votes", Value.fromStringArray(value));
+  }
+
+  get proposal(): string {
+    let value = this.get("proposal");
+    return value.toString();
+  }
+
+  set proposal(value: string) {
+    this.set("proposal", Value.fromString(value));
+  }
+
+  get molochAddress(): Bytes {
+    let value = this.get("molochAddress");
+    return value.toBytes();
+  }
+
+  set molochAddress(value: Bytes) {
+    this.set("molochAddress", Value.fromBytes(value));
+  }
+}
+
+export class Member extends Entity {
+  constructor(id: string) {
+    super();
+    this.entries = new Array(0);
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save Member entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save Member entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("Member", id.toString(), this);
+  }
+
+  static load(id: string): Member | null {
+    return store.get("Member", id) as Member | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get delegateKey(): Bytes {
+    let value = this.get("delegateKey");
+    return value.toBytes();
+  }
+
+  set delegateKey(value: Bytes) {
+    this.set("delegateKey", Value.fromBytes(value));
+  }
+
+  get shares(): BigInt {
+    let value = this.get("shares");
+    return value.toBigInt();
+  }
+
+  set shares(value: BigInt) {
+    this.set("shares", Value.fromBigInt(value));
+  }
+
+  get isActive(): boolean {
+    let value = this.get("isActive");
+    return value.toBoolean();
+  }
+
+  set isActive(value: boolean) {
+    this.set("isActive", Value.fromBoolean(value));
+  }
+
+  get highestIndexYesVote(): BigInt {
+    let value = this.get("highestIndexYesVote");
+    return value.toBigInt();
+  }
+
+  set highestIndexYesVote(value: BigInt) {
+    this.set("highestIndexYesVote", Value.fromBigInt(value));
+  }
+
+  get tokenTribute(): BigInt {
+    let value = this.get("tokenTribute");
+    return value.toBigInt();
+  }
+
+  set tokenTribute(value: BigInt) {
+    this.set("tokenTribute", Value.fromBigInt(value));
+  }
+
+  get didRagequit(): boolean {
+    let value = this.get("didRagequit");
+    return value.toBoolean();
+  }
+
+  set didRagequit(value: boolean) {
+    this.set("didRagequit", Value.fromBoolean(value));
+  }
+
+  get votes(): Array<string> {
+    let value = this.get("votes");
+    return value.toStringArray();
+  }
+
+  set votes(value: Array<string>) {
+    this.set("votes", Value.fromStringArray(value));
+  }
+
+  get submissions(): Array<string> {
+    let value = this.get("submissions");
+    return value.toStringArray();
+  }
+
+  set submissions(value: Array<string>) {
+    this.set("submissions", Value.fromStringArray(value));
+  }
+
+  get molochAddress(): Bytes {
+    let value = this.get("molochAddress");
+    return value.toBytes();
+  }
+
+  set molochAddress(value: Bytes) {
+    this.set("molochAddress", Value.fromBytes(value));
+  }
+}
